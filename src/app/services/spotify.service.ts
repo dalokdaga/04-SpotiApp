d69cf32c63357a79f9b3fdccd3f01aaff4a75d70
--global <dgarcia@grupomabarak.com.mx>
2021-04-16 10:53:07 -0500

    avance hasta el video 11

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -4,19 +4,27 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class SpotifyService {
 
-  constructor(private http: HttpClient) { 
+  constructor(private http: HttpClient) {
     console.log('spotify service listo')
   }
 
   getNewReleases() {
 
     const headers = new HttpHeaders({
-      'Authorization':'Bearer BQBsJCyEeIyE4Qf5l58VORN07g7e26UMRXVMqkwgvbiVPAILgu9dUF1iNKi7uQU7pmPx-IzsueiFoP68Hgw'
+      'Authorization':'Bearer BQBqF-tfAvqOKYC3xUkxxVYPF2vuAczBbtU-JscCNzV3wNdeLFGNy74sX8ZKXQy4MeibjhDbae5-8jsUfCM'
     });
 
     return this.http.get('https://api.spotify.com/v1/browse/new-releases',{headers});
       // .subscribe( (data: any) => {
       //   console.log(data);
       // });
+  }
+
+  getArtista( termino: string){
+    const headers = new HttpHeaders({
+      'Authorization':'Bearer BQBqF-tfAvqOKYC3xUkxxVYPF2vuAczBbtU-JscCNzV3wNdeLFGNy74sX8ZKXQy4MeibjhDbae5-8jsUfCM'
+    });
+
+    return this.http.get(`https://api.spotify.com/v1/search?q=${ termino }&type=artist&limit=20&offset=5`,{headers});
   }
 }
