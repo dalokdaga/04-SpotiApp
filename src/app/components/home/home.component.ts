fec4a92276bd9e6e35d28feeac9ff8a189e7c7c1
--global <dgarcia@grupomabarak.com.mx>
2021-04-16 13:36:21 -0500

    avance hasta el video 13

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -12,11 +12,11 @@ export class HomeComponent{
   nuevasCanciones: any[] = [];
 
   constructor( private spotify: SpotifyService ) {
-   
+
     this.spotify.getNewReleases()
       .subscribe( (data:any)=>{
-        console.log(data.albums.items);
-        this.nuevasCanciones = data.albums.items;
+        console.log(data);
+        this.nuevasCanciones = data;
       });
   }
   // ngOnInit() {
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from "rxjs/operators";
 
 @Injectable()
 export class SpotifyService {
@@ -8,23 +9,26 @@ export class SpotifyService {
     console.log('spotify service listo')
   }
 
-  getNewReleases() {
+  getQuery( query: string){
+    const url = `https://api.spotify.com/v1/${ query }`;
 
     const headers = new HttpHeaders({
-      'Authorization':'Bearer BQBqF-tfAvqOKYC3xUkxxVYPF2vuAczBbtU-JscCNzV3wNdeLFGNy74sX8ZKXQy4MeibjhDbae5-8jsUfCM'
+      'Authorization':'Bearer BQD_NPLry-Q72Idq-M8LXHuZMfVvPRgTrDY3d4V0zzCSyVZjrvigwEmllwfxAogxHEbS7yPgnMjWvytq-3w'
     });
 
-    return this.http.get('https://api.spotify.com/v1/browse/new-releases',{headers});
-      // .subscribe( (data: any) => {
-      //   console.log(data);
-      // });
+    return this.http.get(url,{headers});
+  }
+
+  getNewReleases() {
+
+    return this.getQuery('browse/new-releases')
+          .pipe( map( data =>data['albums'].items));
   }
 
   getArtista( termino: string){
-    const headers = new HttpHeaders({
-      'Authorization':'Bearer BQBqF-tfAvqOKYC3xUkxxVYPF2vuAczBbtU-JscCNzV3wNdeLFGNy74sX8ZKXQy4MeibjhDbae5-8jsUfCM'
-    });
+    // return this.http.get(`https://api.spotify.com/v1/search?q=${ termino }&type=artist&limit=20&offset=5`,{headers});
 
-    return this.http.get(`https://api.spotify.com/v1/search?q=${ termino }&type=artist&limit=20&offset=5`,{headers});
+    return this.getQuery(`search?q=${ termino }&type=artist&limit=20`)
+          .pipe(map(data => data['artists'].items));
   }
 }
